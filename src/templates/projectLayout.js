7f9f193666f093f0123a8c566b776ef71ed76d14
Karol Ryska <karolryska@gmail.com>
2021-10-21 20:18:42 +0200

    feat: import page title from query result

--- src/templates/projectLayout.js
+++ src/templates/projectLayout.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Layout from "./Layout/Layout";
+import { graphql } from "gatsby";
 
-const projectLayout = () => (
-    <Layout title="project" />
+const projectLayout = ({ data }) => (
+    <Layout title={data.mdx.frontmatter.title} />
 );
+
+export const query = graphql`
+    query Title($id: String!) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+            }
+        }
+    }
+`;
 
 export default projectLayout;
