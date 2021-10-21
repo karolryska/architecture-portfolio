6d7124ded8df8b7622c4d093636f83fd833663e5
Karol Ryska <karolryska@gmail.com>
2021-10-21 20:17:53 +0200

    feat: add statement to createPage based on query result

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,17 @@
-const path = require("path")
+const path = require("path");
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   return graphql(`
     query {
-        allMdx(filter: {frontmatter: {type: {eq: "projects"}}}) {
+        allMdx {
             nodes {
                 id
                 frontmatter {
                     title
+                    path
+                    type
                 }
             }
         }
@@ -18,15 +20,24 @@ exports.createPages = ({ graphql, actions }) => {
         if (result.errors) {
             throw result.errors
         };
-        result.data.allMdx.edges.forEach(edge => {
-            console.log(edge.node.frontmatter.pat)
+        result.data.allMdx.nodes.forEach(node => {
+            if (node.frontmatter.type === "project") {
+                createPage({
+                    path: `/projects/${node.frontmatter.path}`,
+                    component: path.resolve("./src/templates/projectLayout.js"),
+                    context: {
+                        id: node.id
+                    }
+                });
+            } else if (node.frontmatter.type === "interior") {
+                createPage({
+                    path: `/interiors/${node.frontmatter.path}`,
+                    component: path.resolve("./src/templates/projectLayout.js"),
+                    context: {
+                        id: node.id
+                    }
+                });
+            };
         });
-
-        result.data.allMdx.edges.forEach(edge => {
-            createPage({
-                path: `/projects/${edge.node.frontmatter.pat}`,
-                component: path.resolve("./src/templates/projectLayout.js")
-            })
-        })
-  })
+  });
 };
