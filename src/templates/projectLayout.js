93ebf9d39a8101458312ebef5fbf9c1710a0df29
Karol Ryska <karolryska@gmail.com>
2021-10-31 00:48:45 +0200

    feat: add images query and pass data to Gallery component

--- src/templates/projectLayout.js
+++ src/templates/projectLayout.js
@@ -1,16 +1,31 @@
 import React from "react";
 import Layout from "./Layout/Layout";
 import { graphql } from "gatsby";
+import Gallery from "../components/Gallery";
 
-const projectLayout = ({ data }) => (
-    <Layout title={data.mdx.frontmatter.title} />
-);
+const projectLayout = ({ data, pageContext }) => {
+    console.log(pageContext);
+    return (
+        <Layout title={data.mdx.frontmatter.title}>
+            <Gallery images={data.allFile.nodes}></Gallery>
+        </Layout>
+    );
+}
 
 export const query = graphql`
-    query Title($id: String!) {
+    query Title($id: String!, $dir: String) {
         mdx(id: {eq: $id}) {
             frontmatter {
                 title
+            }
+            slug
+        }
+        allFile(filter: {extension: {eq: "jpg"}, relativeDirectory: {eq: $dir}}) {
+            nodes {
+                id
+                childImageSharp {
+                    gatsbyImageData
+                }
             }
         }
     }
