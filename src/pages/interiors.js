bf19dd4e85540cde3e493e313050fc0a316cd4d3
Karol Ryska <karolryska@gmail.com>
2021-10-31 13:40:14 +0100

    feat: add projects query

--- src/pages/interiors.js
+++ src/pages/interiors.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from "../templates/Layout/Layout";
 import ProjectsList from '../components/ProjectsList';
 
 const InteriorsPage = () => {
+    const data = useStaticQuery(graphql`{
+        allMdx(filter: {frontmatter: {type: {eq: "interior"}}}) {
+            nodes {
+                id
+                frontmatter {
+                    path
+                    hero_image {
+                        childImageSharp {
+                            gatsbyImageData
+                        }   
+                    }
+                }
+            }
+        }
+    }`);
+
     return (
         <Layout title="Interiors">
-            <ProjectsList />
+            <ProjectsList projects={data.allMdx.nodes} />
         </Layout>
     )
 };
