92d3c10cc52ec949786b90a218580831049f6414
Karol Ryska <karolryska@gmail.com>
2021-10-31 21:39:13 +0100

    feat: add main image to gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getImage } from 'gatsby-plugin-image';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import styled from 'styled-components';
+
+const gridGap = 10;
+const calcImageHeight = (containerWidth) => {
+    const imageWidth = (containerWidth - 2 * gridGap) / 3;
+    return (imageWidth / 3) * 2;
+};
 
 const Wrapper = styled.div`
     display: flex;
@@ -12,43 +18,60 @@ const Wrapper = styled.div`
 const MainImage = styled.div`
     height: 400px;
     width: 100%;
-    margin-bottom: 20px;
+    margin-bottom: ${gridGap}px;
     background-color: lightgray;
 `;
+
+console.log(MainImage.inner);
 
 const ImagesContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 20px;
+    grid-gap: ${gridGap}px;
     width: 100%;
 `;
 
 const Image = styled.div`
-    height: 100px;
+    height: ${calcImageHeight(600)}px;
     background-color: lightgray;
 `;
 
-const Gallery = ({ images }) => (
-    <Wrapper>
-        <MainImage />
-        <ImagesContainer>
-            {images.map((item) => {
-                const image = getImage(item);
-                return (
-                    <Image key={item.id}>
-                        <GatsbyImage
-                            image={image}
-                            alt='image'
-                            style={{
-                                width: '100%',
-                                height: '100%',
-                            }}
-                        />
-                    </Image>
-                );
-            })}
-        </ImagesContainer>
-    </Wrapper>
-);
+const Gallery = ({ images }) => {
+    const [mainImage, setMainImage] = useState(images[0]);
+    const handleClick = (index) => {
+        setMainImage(images[index]);
+    };
+    return (
+        <Wrapper>
+            <MainImage>
+                <GatsbyImage
+                    image={getImage(mainImage)}
+                    alt='image'
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                    }}
+                />
+            </MainImage>
+            <ImagesContainer>
+                {images.map((item, i) => {
+                    const image = getImage(item);
+                    return (
+                        <Image key={item.id} onClick={() => handleClick(i)}>
+                            <GatsbyImage
+                                image={image}
+                                alt='image'
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                }}
+                            />
+                        </Image>
+                    );
+                })}
+            </ImagesContainer>
+        </Wrapper>
+    );
+};
 
 export default Gallery;
