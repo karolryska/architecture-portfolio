b1c367d270300bfe57a5fa503f654bde03aeb71b
Karol Ryska <karolryska@gmail.com>
2021-11-06 21:24:49 +0100

    feat: add logotype on page loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Layout from '../templates/Layout/Layout';
+import Logo from '../components/Logo';
 
 const IndexPage = ({ location }) => {
-    return (
-        <Layout title='Featured'>
-            {location.state && !location.state.logo ? (
-                <p>no animation</p>
-            ) : (
-                <p>animate logo</p>
-            )}
-        </Layout>
-    );
+    const [logoIsActive, setLogoIsActive] = useState();
+    const logoTimeout = () => {
+        setLogoIsActive(true);
+        setTimeout(() => setLogoIsActive(false), 3000);
+    };
+
+    useEffect(() => {
+        location.state ? setLogoIsActive(false) : logoTimeout();
+    }, []);
+    return <>{logoIsActive ? <Logo /> : <Layout title='Featured'></Layout>}</>;
 };
 
 export default IndexPage;
