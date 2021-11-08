bb4b500fe9ae9046a726b119667358e238a8764f
Karol Ryska <karolryska@gmail.com>
2021-11-08 22:00:57 +0100

    feat: add animation and make size depending on props

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Wrapper = styled.div`
-    position: fixed;
+    position: ${(props) => props.animation && 'fixed'};
     top: 50vh;
     left: 50vw;
     display: grid;
     grid-template-rows: 1fr 1fr;
-    grid-template-columns: 50px 50px 50px 25px 50px;
-    height: 100px;
-    transform: translate(-50%, -50%);
+    grid-template-columns: repeat(3, 1fr) 0.5fr 1fr;
+    height: ${(props) => props.height}px;
+    width: ${(props) => props.height * 2.25}px;
+    transform: ${(props) => props.animation && 'translate(-50%, -50%)'};
     overflow: hidden;
 `;
 
-const Vertical = styled.div`
+const Vertical = styled(motion.div)`
     grid-column-start: 2;
     grid-row-start: 1;
     grid-row-end: 3;
     background-color: black;
 `;
 
-const Horizontal = styled.div`
+const verticalVariants = {
+    initial: { y: -200 },
+    animate: { y: 0 },
+};
+
+const Horizontal = styled(motion.div)`
     grid-column-start: 1;
     grid-column-end: 4;
     grid-row-start: 1;
@@ -28,17 +35,43 @@ const Horizontal = styled.div`
     background-color: black;
 `;
 
-const Dot = styled.div`
+const horizontalVariants = {
+    initial: { x: -200 },
+    animate: { x: 0 },
+};
+
+const Dot = styled(motion.div)`
     grid-column-start: 5;
     grid-row-start: 2;
     background-color: black;
 `;
 
-const Logo = () => (
-    <Wrapper>
-        <Vertical />
-        <Horizontal />
-        <Dot />
+const dotVariants = {
+    initial: { scaleX: 0 },
+    animate: { scaleX: 1 },
+};
+
+const Logo = ({ animation, height }) => (
+    <Wrapper animation={animation} height={height}>
+        <Vertical
+            variants={animation && verticalVariants}
+            initial='initial'
+            animate='animate'
+            transition={{ duration: 0.2 }}
+        />
+        <Horizontal
+            variants={animation && horizontalVariants}
+            initial='initial'
+            animate='animate'
+            transition={{ duration: 0.2, delay: 0.2 }}
+        />
+        <Dot
+            variants={animation && dotVariants}
+            initial='initial'
+            animate='animate'
+            style={{ originX: 0 }}
+            transition={{ duration: 0.2, delay: 0.4 }}
+        />
     </Wrapper>
 );
 
