72f38ba8626cd55dae6aa3f2f7617f9e55a7df54
Karol Ryska <karolryska@gmail.com>
2021-11-10 23:18:43 +0100

    chore: change animation handler

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,18 +3,16 @@ import React, { useState } from 'react';
 import Layout from '../templates/Layout/Layout';
 import Logo from '../components/Logo';
 
-const IndexPage = ({ location }) => {
-    const [logoIsActive, setLogoIsActive] = useState(() =>
-        !location.state ? true : false
-    );
+const IndexPage = () => {
+    const [logoIsActive, setLogoIsActive] = useState(true);
     setTimeout(() => setLogoIsActive(false), 2000);
 
     return (
         <>
             {logoIsActive ? (
-                <Logo animation='true' height={100} />
+                <Logo animation={true} height={100} />
             ) : (
-                <Layout title='Featured'></Layout>
+                <Layout title='Featured' animation={true}></Layout>
             )}
         </>
     );
