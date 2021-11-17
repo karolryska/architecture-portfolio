47bb7b26dfcad4c751af4caa4570f95ee9fb212e
Karol Ryska <karolryska@gmail.com>
2021-11-17 23:47:12 +0100

    feat: add <p> with project description

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import useWidth from '../hooks/useWidth';
@@ -15,7 +16,17 @@ const Section = styled.section`
     }
 `;
 
-const SectionTitle = styled(motion.h2)`
+const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    @media (min-width: 769px) {
+        margin: 0 80px 0 6%;
+    }
+`;
+
+const Title = styled(motion.h2)`
     align-self: flex-start;
     position: relative;
     margin: 0 0 14px;
@@ -34,21 +45,48 @@ const SectionTitle = styled(motion.h2)`
         background-color: black;
         z-index: 10;
     }
+`;
 
-    @media (min-width: 769px) {
-        margin: 0 0 0 8%;
-    }
+const Description = styled(motion.p)`
+    text-align: left;
 `;
 
 const Content = styled.article`
     min-height: 100%;
+
+    @media (min-width: 769px) {
+        width: 60%;
+    }
 `;
 
-const MainContent = ({ children, title }) => {
+const contentTransition = {
+    duration: 0.2,
+    delay: 0.2,
+};
+
+const contentTransitionDelay = {
+    duration: 0.2,
+    delay: 2.7,
+};
+
+const MainContent = ({ children, title, description }) => {
     const [isMobile] = useWidth();
     return (
         <Section isMobile={isMobile}>
-            <SectionTitle isMobile={isMobile}>{title}</SectionTitle>
+            <Header>
+                <Title
+                    isMobile={isMobile}
+                    initial={{ y: -20, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={contentTransition}>
+                    {title}
+                </Title>
+                {description && (
+                    <Description>
+                        <MDXRenderer>{description}</MDXRenderer>
+                    </Description>
+                )}
+            </Header>
             <Content>{children}</Content>
         </Section>
     );
