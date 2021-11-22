fa1708f066a372acd77fa2035b14ffbcdebc4f30
Karol Ryska <karolryska@gmail.com>
2021-11-22 23:10:36 +0100

    feat: clarify elements size

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import { getImage } from 'gatsby-plugin-image';
 
 import ProjectLink from './ProjectLink';
 
 const Wrapper = styled.ul`
-    display: inline-grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 16px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin-top: 20px;
+
+    @media (min-width: 769px) {
+        display: inline-grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-auto-rows: ${(props) => (props.wrapperWidth - 32) / 3}px;
+        grid-gap: 16px;
+    }
 `;
 
-const ProjectsList = ({ projects }) => (
-    <Wrapper>
-        {projects.map((project) => {
-            const image = getImage(project.frontmatter.hero_image);
-            const path = project.frontmatter.path;
-            return <ProjectLink key={path} image={image} slug={path} />;
-        })}
-    </Wrapper>
-);
+const Placehholder = styled.div`
+    height: 100%;
+    width: 100%;
+    background-color: lightgrey;
+`;
+
+const ProjectsList = ({ projects }) => {
+    const [wrapperWidth, setWrapperWidth] = useState();
+    const wrapperRef = useRef();
+
+    useEffect(() => {
+        wrapperRef.current && setWrapperWidth(wrapperRef.current.offsetWidth);
+    }, []);
+
+    return (
+        <Wrapper ref={wrapperRef} wrapperWidth={wrapperWidth}>
+            {projects.map((project) => {
+                const image = getImage(project.frontmatter.hero_image);
+                const path = project.frontmatter.path;
+                return (
+                    <ProjectLink
+                        key={path}
+                        image={image}
+                        slug={path}
+                        wrapperWidth={wrapperWidth}
+                    />
+                );
+            })}
+            <Placehholder />
+            <Placehholder />
+            <Placehholder />
+            <Placehholder />
+            <Placehholder />
+            <Placehholder />
+            <Placehholder />
+        </Wrapper>
+    );
+};
 
 export default ProjectsList;
