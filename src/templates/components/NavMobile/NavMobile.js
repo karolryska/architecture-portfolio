5b0144e51d21b87b08080f99d9a8fa388e9fa064
Karol Ryska <karolryska@gmail.com>
2021-11-25 22:35:15 +0100

    feat: add button and change link color

--- src/templates/components/NavMobile/NavMobile.js
+++ src/templates/components/NavMobile/NavMobile.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import { AnimatePresence } from 'framer-motion';
 import { Wrapper, NavList, NavItem } from './NavMobile.styles';
+import Button from '../Button/Button';
 
 const NavMobile = ({ paths }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,7 +10,7 @@ const NavMobile = ({ paths }) => {
 
     return (
         <>
-            <button onClick={handleClick}>menu</button>
+            <Button onClick={handleClick} isActive={isOpen} />
             <AnimatePresence>
                 {isOpen && (
                     <Wrapper
@@ -22,6 +23,7 @@ const NavMobile = ({ paths }) => {
                                 <NavItem onClick={handleClick} key={item[1]}>
                                     <Link
                                         to={`/${item[0]}`}
+                                        style={{ color: 'black' }}
                                         state={{ animation: false }}>
                                         {item[1]}
                                     </Link>
