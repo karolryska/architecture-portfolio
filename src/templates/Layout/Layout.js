54b43c81ac76aaebde0624b412d4ee1a68fd4afd
Karol Ryska <karolryska@gmail.com>
2021-12-02 23:59:05 +0100

    src/theme/
    feat: add theme component

--- src/templates/Layout/Layout.js
+++ src/templates/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styles from './Layout.module.css';
+import Theme from '../../theme/theme';
 import useWidth from '../../hooks/useWidth';
 import { Wrapper, Header, Main, Footer } from './Layout.styles';
 import Logo from '../../components/Logo/Logo';
@@ -18,25 +19,20 @@ const Layout = ({ children }) => {
     const [isMobile] = useWidth();
 
     return (
-        <Wrapper>
-            <Header
-                initial={{ y: -50, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ duration: 0.2, delay: 2.5 }}>
-                <Link to='/' state={{ logo: false }}>
-                    <Logo height={50} />
-                </Link>
-                {isMobile ? (
-                    <NavMobile paths={paths} />
-                ) : (
-                    <NavDesktop paths={paths} />
-                )}
-            </Header>
-            <Main>{children}</Main>
-            <Footer>
-                <p>code by</p>
-            </Footer>
-        </Wrapper>
+        <Theme>
+            <Wrapper>
+                <Header initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.2, delay: 2.5 }}>
+                    <Link to='/' state={{ logo: false }}>
+                        <Logo height={50} />
+                    </Link>
+                    {isMobile ? <NavMobile paths={paths} /> : <NavDesktop paths={paths} />}
+                </Header>
+                <Main>{children}</Main>
+                <Footer>
+                    <p>code by</p>
+                </Footer>
+            </Wrapper>
+        </Theme>
     );
 };
 
