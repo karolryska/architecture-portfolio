f4a3395926f874bcfbc9caa803e5524692341749
Karol Ryska <karolryska@gmail.com>
2021-12-04 20:28:31 +0100

    feat: add title prop to project link component

--- src/components/ProjecstList/ProjectsList.js
+++ src/components/ProjecstList/ProjectsList.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { getImage } from 'gatsby-plugin-image';
 import { Wrapper, Placeholder } from './ProjectsList.styles';
 import ProjectLink from '../ProjectLink/ProjectLink';
+import formatIndex from '../../helpers.js/formatIndex';
 
 const ProjectsList = ({ projects }) => {
     const [wrapperWidth, setWrapperWidth] = useState();
@@ -13,17 +14,10 @@ const ProjectsList = ({ projects }) => {
 
     return (
         <Wrapper ref={wrapperRef} wrapperWidth={wrapperWidth}>
-            {projects.map((project) => {
+            {projects.map((project, i) => {
                 const image = getImage(project.frontmatter.hero_image);
                 const path = project.frontmatter.path;
-                return (
-                    <ProjectLink
-                        key={path}
-                        image={image}
-                        slug={path}
-                        wrapperWidth={wrapperWidth}
-                    />
-                );
+                return <ProjectLink key={path} image={image} title={formatIndex(i)} slug={path} wrapperWidth={wrapperWidth} />;
             })}
             <Placeholder />
             <Placeholder />
--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
+import Theme from '../../theme/theme';
 import { Wrapper } from './ProjectLink.styles';
 
-const ProjectLink = ({ image, slug }) => {
+const ProjectLink = ({ image, title, slug }) => {
     return (
-        <Wrapper title={slug}>
+        <Theme>
             <Link
                 to={slug}
                 style={{
                     width: '100%',
                     height: '100%',
                 }}>
-                <GatsbyImage
-                    image={image}
-                    alt='image'
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                    }}
-                />
+                <Wrapper title={title}>
+                    <GatsbyImage
+                        image={image}
+                        alt={slug}
+                        style={{
+                            width: '100%',
+                            height: '100%',
+                        }}
+                    />
+                </Wrapper>
             </Link>
-        </Wrapper>
+        </Theme>
     );
 };
 
