5fce12c48234a926a8f09a49873dc7a8733542b7
Karol Ryska <karolryska@gmail.com>
2021-12-13 21:44:17 +0100

    chore: add google fonts package

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,15 +17,19 @@ module.exports = {
         'gatsby-plugin-sharp',
         {
             resolve: `gatsby-plugin-styled-components`,
-            options: {
-                // Add any options here
-            },
         },
         'gatsby-transformer-sharp',
         {
             resolve: `gatsby-plugin-layout`,
             options: {
                 component: require.resolve(`./src/templates/Layout/Layout.js`),
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [`roboto\:200,400,500,700`],
+                display: 'swap',
             },
         },
     ],
