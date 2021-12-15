3ff90ad94b7c30f8534695713f488868a9daa483
Karol Ryska <karolryska@gmail.com>
2021-12-15 21:09:47 +0100

    feat: set styles

--- src/components/AwardsList/AwardsList.styles.js
+++ src/components/AwardsList/AwardsList.styles.js
@@ -1,17 +1,32 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.ul`
-    width: 30vw;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    padding: 0;
+    margin: 10px 0;
     list-style: none;
+
+    @media (min-width: 769px) {
+        margin: 0;
+    }
 `;
 
-export const Award = styled.p`
-    margin-bottom: 16px;
-    font-size: 22px;
+export const Award = styled.li`
+    margin: 0 0 20px;
+    font-size: 20px;
     font-weight: 400;
-    cursor: default;
 
-    &:hover {
-        color: orange;
+    & p {
+        margin: 0;
+    }
+
+    @media (min-width: 769px) {
+        margin: 0 0 26px;
+
+        &:hover {
+            color: ${(props) => props.theme.colors.yellow};
+        }
     }
 `;
