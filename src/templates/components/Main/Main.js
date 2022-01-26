32b957af450e958f02d8742ead11855090a44891
Karol Ryska <karolryska@gmail.com>
2022-01-26 23:35:24 +0100

    feat: add gsap animations

--- src/templates/components/Main/Main.js
+++ src/templates/components/Main/Main.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import gsap from 'gsap';
 import { Section, Header, Title, Description, Content, contentTransition } from './Main.styles';
 
 const Main = ({ children, title, description, fullWidth }) => {
+    const headerRef = useRef(null);
+    const contentRef = useRef(null);
+
+    useEffect(() => {
+        gsap.from(headerRef.current.children, {
+            ...contentTransition,
+            stagger: 0.4,
+        });
+        gsap.from(contentRef.current.children, {
+            ...contentTransition,
+            delay: 0.8,
+        });
+    }, []);
+
     return (
         <Section>
-            <Header>
-                <Title
-                    initial={{ y: -20, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={contentTransition}>
-                    {title}
-                </Title>
+            <Header ref={headerRef}>
+                <Title>{title}</Title>
                 {description && (
                     <Description>
                         <MDXRenderer>{description}</MDXRenderer>
                     </Description>
                 )}
             </Header>
-            <Content fullWidth={fullWidth}>{children}</Content>
+            <Content ref={contentRef} fullWidth={fullWidth}>
+                {children}
+            </Content>
         </Section>
     );
 };
